44c9ce0f8ff16c6028e175ab024f0ee825d18558
stevenmosher <moshersteven@gmail.com>
2016-06-28 07:28:35 -0700

    Added file index

--- Constants.R
+++ Constants.R
@@ -100,7 +100,8 @@ BERKELEY_DATA <- tbl_df(data.frame(Url = c(u1,u2,u3,u4,u5,u6,u7,u8,u9,u10,u11,u1
                                             nchar(Url)))    %>%
                  mutate(Directory = DOWNLOADS,Coverage   = "Land", Area= "Global", TimePeriod  ="Monthly") %>%
                  mutate(Type  = ifelse(grepl("Gridded",Url),"Gridded","Time_Series")) %>%
-                 mutate(Metric =  "TAVG",Processing ="Adjusted")  
+                 mutate(Metric =  "TAVG",Processing ="Adjusted") %>%
+                 mutate(Index=1:n())
                  
                 
                  
