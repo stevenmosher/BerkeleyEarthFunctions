9792df14bd8f273339efe54b4d4285f04f9ced5f
stevenmosher <moshersteven@gmail.com>
2016-06-28 18:04:14 -0700

    added RSQlite

--- Libraries.R
+++ Libraries.R
@@ -1,5 +1,5 @@
 ###################  Libraries used by Functions#########
-PACKAGES <- c("RSQLite","dplyr","data.table","zoo","raster","ncdf4","stringr","pacman")
+PACKAGES <- c("R.utils","RSQLite","dplyr","data.table","zoo","raster","ncdf4","stringr","pacman")
 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(char=PACKAGES)
