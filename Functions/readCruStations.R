6208364dae16f7f906f0d84a1897b54290c2c042
stevenmosher <moshersteven@gmail.com>
2016-07-01 18:10:28 -0700

    Initial

---
+++ Functions/readCruStations.R
@@ -0,0 +1,110 @@
+
+
+readCruStations <- function(inFile, output=c( "Data","Inventory")){
+      require(dplyr)
+      
+     if(length(output)>2)stop(cat("output selection wrong", output, "\n"))
+     output <- tolower(output)
+     matchedO <- sum(output %in% c("data","inventory"))
+     if(matchedO==0)stop(cat("output selection wrong", output, "\n"))
+  
+      X <- readLines(inFile)
+      headerLength <- nchar(X[1])
+      if(headerLength == 83){
+        CRU4 = TRUE
+        CRU3 = FALSE
+      }else{
+        CRU3 = TRUE
+        CRU4 = FALSE
+      }
+      
+      
+      dex <- which(nchar(X[]) == headerLength)
+      headers  <- X[dex]
+      
+      
+      if(CRU3){ 
+
+         Inv <- data.frame(Id  =  as.numeric(substr(headers,1,6)),
+                           Lat = as.numeric(substr(headers,7,10)),
+                           Lon = as.numeric(substr(headers,11,15)),
+                           Altitude = as.numeric(substr(headers,16,20)),
+                           Name = substr(headers,22,42),
+                           Country = substr(headers,43,56),
+                           StartYear = as.numeric(substr(headers,57,60)),
+                           EndYear  =  as.numeric(substr(headers,61,64)),
+                           Source   =  as.numeric(substr(headers,67,68)),
+                           GoodFirst = as.numeric(substr(headers,69,72)),
+                           stringsAsFactors = FALSE)
+
+        Inv$Lat[Inv$Lat == -999]    <- NA
+        Inv$Lon[Inv$Lon == -1999]   <- NA
+        Inv$Lat <- Inv$Lat / 10
+        Inv$Lon <- (-1)*Inv$Lon / 10
+            
+        Inv$Altitude[Inv$Altitude == -999] <- NA
+      
+      } else {
+        Inv <- data.frame(Id  =  as.numeric(substr(headers,1,7)),
+                          Lat = as.numeric(substr(headers,8,11)),
+                          Lon = as.numeric(substr(headers,12,16)),
+                          Altitude = as.numeric(substr(headers,17,21)),
+                          Name = substr(headers,23,43),
+                          Country = substr(headers,44,57),
+                          StartYear = as.numeric(substr(headers,58,61)),
+                          EndYear  =  as.numeric(substr(headers,62,65)),
+                          Source   =  as.numeric(substr(headers,66,69)),
+                          GoodFirst = as.numeric(substr(headers,70,73)),
+                          Index    = as.numeric(substr(headers,74,78)),
+                          GridNumber    = as.numeric(substr(headers,79,83)),
+                          stringsAsFactors = FALSE)
+        
+        Inv$Lat[Inv$Lat == -999]    <- NA
+        Inv$Lon[Inv$Lon == -1999]   <- NA
+        Inv$Lat <- Inv$Lat / 10
+        Inv$Lon <- (-1)*Inv$Lon / 10
+        
+        Inv$Altitude[Inv$Altitude == -999] <- NA
+        Inv <- tbl_df(Inv)
+        
+      }
+      
+      if(matchedO ==1 & output == "inventory")return(Inv)
+
+    begin <- min(Inv$StartYear)
+    end   <- max(Inv$EndYear)
+    Years <- begin:end
+    NoYear <- end-begin+1
+    stations <- nrow(Inv)
+    Id    <- unique(Inv$Id)
+    DATA  <- matrix(NA, ncol=4, nrow = length(Id)*NoYear*12)
+    dimnames(DATA)[2]<- list(c("Id","Year","Month","TAVG"))
+    DATA[,"Id"]<-rep(Id, each=NoYear*12)
+    DATA[,"Year"]<- rep(Years, each=12)
+    DATA[, "Month"]<-1:12
+     
+
+    stationCount <- 0
+    for(line in 1:length(X)){
+         if (nchar(X[line]) == headerLength){
+            # we have a header
+            stationCount <- stationCount + 1
+          } else {
+             # we have a data record
+            thisLine <- as.numeric(unlist(strsplit(X[line]," +")))
+            thisLine[thisLine == -999] <-NA
+            ThisID <- Id[stationCount]
+            thisYear <- thisLine[1]
+            INDEX <- min(intersect(which(DATA[,"Id"] == ThisID),which(DATA[,"Year"]==thisYear)))
+            DATA[INDEX:(INDEX+11),"TAVG"] <- thisLine[2:13]
+     
+          }
+    }
+   if(matchedO == 2)return(list(Data = tbl_df(DATA),Inventory=Inv))
+   if(matchedO ==1 & output == "data")return(tbl_df(DATA))
+           
+       
+}
+
+ 
+ 
