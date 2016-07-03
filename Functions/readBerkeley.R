fb36ad2ba3e263e0ff121d109db4e5edff1cf627
stevenmosher <moshersteven@gmail.com>
2016-07-03 10:52:55 -0700

    added calls to readStation

--- Functions/readBerkeley.R
+++ Functions/readBerkeley.R
@@ -3,23 +3,23 @@ readBerkeley  <- function(filename, Extent = NULL){
   if(!file.exists(filename))stop(cat(filename, " Does not exist","\n"))
   
   NetCDF <- grepl(pattern = "\\.nc",filename)
-  if(NetCDF) return(readBerkeleyNetCDF(filename = fname, Extent=Extent,start=start,end=end))
+  if(NetCDF) return(readBerkeleyNetCDF(filename = fname, Extent=Extent))
   
   ###   Not a netcdf 
-  switch(filename,
+  switch(basename(filename),
          
-          data.txt=                      { print("hi")},
-          site.summary.txt =             {print("by")},
-          data_characterization.txt   = {},
-          data_flag_definitions.txt   = {},
-          flags.txt                   = {},
-          site_complete_detail.txt    = {},
-          site_detail.txt             = {},
-          site_flag_definitions.txt   = {},
-          site_flags.txt              = {},
-          source_flag_definitions.txt  = {},
-          sources.txt                  = {},
-          station_change.txt            = {},
+          data.txt                     = {return(readBerkeleyStation(filename))},
+          site.summary.txt             = {return(readBerkeleyStation(filename))},
+          data_characterization.txt    = {return(readBerkeleyStation(filename))},
+          data_flag_definitions.txt    = {return(readBerkeleyStation(filename))},
+          flags.txt                    = {return(readBerkeleyStation(filename))},
+          site_complete_detail.txt     = {return(readBerkeleyStation(filename))},
+          site_detail.txt              = {return(readBerkeleyStation(filename))},
+          site_flag_definitions.txt    = {return(readBerkeleyStation(filename))},
+          site_flags.txt               = {return(readBerkeleyStation(filename))},
+          source_flag_definitions.txt  = {return(readBerkeleyStation(filename))},
+          sources.txt                  = {return(readBerkeleyStation(filename))},
+          station_change.txt           = {return(readBerkeleyStation(filename))},
           Land_and_Ocean_complete.txt  = {return(readBerkeleySummary(fname))},
           Complete_TAVG_complete.txt   = {return(readBerkeleySummary(fname))},
           Complete_TMAX_complete.txt   = {return(readBerkeleySummary(fname))},
