5accd6c1bc7845e78d89b92949d32b0b7f027450
stevenmosher <moshersteven@gmail.com>
2016-07-03 17:45:43 -0700

    fix sourcing

--- Initialize.R
+++ Initialize.R
@@ -1,8 +1,5 @@
 Initialize <- function(){
   source("Libraries.R")
-  source("Constants.R")
-  functions <- list.files(FUNCTIONS,full.names=T, pattern = "\\.R")
-  if(length(functions)>0)lapply(functions, FUN=source)
-  ####  Create  database of downloads
+   
   return(getwd())
 }
