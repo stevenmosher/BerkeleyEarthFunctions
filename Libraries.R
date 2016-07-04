b5adcfaf619f4e89dc573d08fbb82624085de769
stevenmosher <moshersteven@gmail.com>
2016-07-03 17:46:19 -0700

    need to add purrr and others

--- Libraries.R
+++ Libraries.R
@@ -1,5 +1,5 @@
 ###################  Libraries used by Functions#########
-PACKAGES <- c("R.utils","RSQLite","dplyr","data.table","zoo","raster","ncdf4","stringr","pacman")
+PACKAGES <- c("R.utils","RSQLite","dplyr","data.table","zoo","raster","ncdf4","stringr","pacman","readr")
 
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(char=PACKAGES)
